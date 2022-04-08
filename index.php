3c9e85b22f87580ee4bdfc1d8a4f2f7614fc970d
Miyka96 <micaelaseminara@outlook.it>
2022-04-08 21:53:33 +0200

    adding some content just for fun

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     $dolceVita = new Film("La dolce vita","Federico Fellini",1960,"./img/dolce_vita.jpg");
     $psycho = new Film("Psycho", "Alfred Hitchcock", 1960, "./img/psycho.jpg");
     $persona = new Film("Persona","Ingmar Bergman", 1966,"./img/persona.jpg");
+    $lobster = new Film("The Lobster", "Jorgos Lanthimos", 2015,"./img/lobster.jpg");
+    $cuculo = new Film("Qualcuno volò sul nido del cuculo", "Milos Forman", 1975, "./img/cuculo.jpeg");
+    $shindler = new Film("Shindler's List", "Steven Spielberg", 1993, "./img/shindler.jpg");
+    $nosferatu = new Film("Nosferatu il Vampiro", "Friedrich Wilhelm Murnau", 1922, "./img/nosferatu.jpg");
 ?>
 
 <!DOCTYPE html>
@@ -28,6 +32,18 @@
         </li>
         <li>
             <?php echo $persona->getFullDescription(); ?>
+        </li>
+        <li>
+            <?php echo $lobster->getFullDescription(); ?>
+        </li>
+        <li>
+            <?php echo $cuculo->getFullDescription(); ?>
+        </li>
+        <li>
+            <?php echo $shindler->getFullDescription(); ?>
+        </li>
+        <li>
+            <?php echo $nosferatu->getFullDescription(); ?>
         </li>
         <br>
     </ul>
